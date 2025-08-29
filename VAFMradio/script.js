58c4256aca1a0a448f6bef4dc50c7156dd8d0f42
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2025-08-29 20:32:49 +0200

    Delete VAFMradio directory

--- VAFMradio/script.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:5000/api/content/latest')
-  .then(res => res.json())
-  .then(data => {
-    document.body.style.backgroundColor = data.couleurFond;
-    document.getElementById('titre').innerText = data.titre;
-    document.getElementById('texte').innerText = data.texte;
-    document.getElementById('image').src = data.imageURL;
-  })
-  .catch(err => console.error("Erreur chargement contenu :", err));

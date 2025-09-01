0375237c57146a7bc7143dc15a9a8ec51a985721
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2025-09-01 19:55:08 +0200

    Update principal.js

--- principal.js
+++ principal.js
@@ -5,3 +5,11 @@ function toggleMenu() {
   menu.classList.toggle("open");
   toggleBtn.classList.toggle("open");
 }
+
+  function openPopup() {
+    document.getElementById("popup").classList.remove("hidden");
+  }
+
+  function closePopup() {
+    document.getElementById("popup").classList.add("hidden");
+  }

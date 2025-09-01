1c31b6642d967aa3ca4c9eb5234177f0956dc99f
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2025-09-01 15:37:34 +0200

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
   const menu = document.getElementById("sideMenu");
+
+  // Si le menu est ouvert, on le remet complètement hors écran
   if (menu.style.right === "0px") {
-    menu.style.right = "-250px";
+    menu.style.right = "-250px"; // complètement caché
   } else {
-    menu.style.right = "0px";
+    menu.style.right = "0px"; // complètement visible
   }
 }
+

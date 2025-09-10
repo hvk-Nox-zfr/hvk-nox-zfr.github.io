a6013f3412db3fcbfc98041e7b3daa2229e713b7
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2025-09-10 19:49:17 +0200

    Update principal.js

--- principal.js
+++ principal.js
@@ -168,7 +168,8 @@ dedicaceForm.addEventListener("submit", e => {
   const blacklist = [
   "con", "connard", "connasse", "merde", "putain", "salope", "enculé", "fdp", "ntm", "tg",
   "ta gueule", "nique", "batard", "bâtard", "bite", "couille", "pétasse", "enfoiré", "gros con",
-  "fils de", "chier", "débile", "abruti", "crétin", "dégueulasse", "slp", "putain", "hugo", "salope"
+  "fils de", "chier", "débile", "abruti", "crétin", "dégueulasse", "slp", "putain", "hugo", "salope",
+  "trou de balle"
   ];
   const contientGrosMot = blacklist.some(mot => message.toLowerCase().includes(mot));
 
